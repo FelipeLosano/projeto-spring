5075b7a915c9814e6a47f46f87fa1bbe355a8b51
Felipe Losano <felipelosano@outlook.com>
2024-01-01 22:42:08 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/myproject/projetospring/entities/Category.java
+++ src/main/java/com/myproject/projetospring/entities/Category.java
@@ -1,5 +1,6 @@
 package com.myproject.projetospring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -20,8 +21,8 @@ public class Category implements Serializable {
   private Long id;
   private String name;
 
-//  @OneToMany
-  @Transient
+  @ManyToMany(mappedBy = "categories")
+  @JsonIgnore
   private Set<Product> products = new HashSet<>();
 
   public Category() {
--- src/main/java/com/myproject/projetospring/entities/Product.java
+++ src/main/java/com/myproject/projetospring/entities/Product.java
@@ -22,8 +22,8 @@ public class Product implements Serializable {
   private Double price;
   private String imgUrl;
 
-  //  @OneToMany
-  @Transient
+  @ManyToMany
+  @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
   private Set<Category> categories = new HashSet<>();
 
   public Product() {
