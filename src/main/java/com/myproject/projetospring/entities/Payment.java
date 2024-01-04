505df9e9c0743cec46ffb22a04a896d324104d4a
Felipe Losano <felipelosano@outlook.com>
2024-01-03 21:19:08 -0300

    Subtotal & Total methods

--- src/main/java/com/myproject/projetospring/entities/Payment.java
+++ src/main/java/com/myproject/projetospring/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.myproject.projetospring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -20,6 +21,7 @@ public class Payment implements Serializable {
   private Long id;
   private Instant moment;
 
+  @JsonIgnore
   @OneToOne
   @MapsId
   private Order order;
@@ -31,6 +33,30 @@ public class Payment implements Serializable {
   public Payment(Long id, Instant moment, Order order) {
     this.id = id;
     this.moment = moment;
+    this.order = order;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Instant getMoment() {
+    return moment;
+  }
+
+  public void setMoment(Instant moment) {
+    this.moment = moment;
+  }
+
+  public Order getOrder() {
+    return order;
+  }
+
+  public void setOrder(Order order) {
     this.order = order;
   }
 
